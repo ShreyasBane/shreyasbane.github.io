824b535523230e8a8e29c0e737935ee6b555488c
ShreyasBane <shreyaspbane@gmail.com>
2022-06-14 22:26:44 +0530

    Cleaning the slate

--- pages/index.js
+++ pages/index.js
@@ -5,20 +5,10 @@ import NavLinks from '../components/NavLinks';
 import Footer from '../components/Footer';
 import TopBar from '../components/TopBar';
 
-// /blog/test
-// /pages/blog/[slug].js
-
 export default function Home() {
   return (
-    <div>
-      <div className='w-full flex flex-col items-center'>
-        <TopBar />
-        <img src='/profile.jpeg' className='w-40 m-6 rounded-full'></img>
-        <h1 className='text-4xl title-font'>Shreyas Bane</h1>
-        <h3 className='m-4 text-xl regular-font'>Software Developer and hobby artist</h3>
-        <NavLinks />
-      </div>
-      <Footer />
+    <div className=''>
+      <h1>Hello</h1>
     </div>
   )
 }
